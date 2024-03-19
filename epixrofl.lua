6e6d8f4944640d7ce053415dde0830fda6dfa9fb
Unbl0xed <160192405+Unbl0xed@users.noreply.github.com>
2024-03-19 16:53:05 -0500

    Update epixrofl.lua

--- epixrofl.lua
+++ epixrofl.lua
@@ -10462,7 +10462,7 @@ PL:NewDropdown("Give Gun", "Give urself a gun!!!11", {"M9", "Remington 870", "AK
     local A_1 = game:GetService("Workspace")["Prison_ITEMS"].giver[currentOption].ITEMPICKUP
     local event = game:GetService("Workspace").Remote.ItemHandler
     print(A_1.Name)
-    print(currentChoice)
+    print(currentOption)
     event:InvokeServer(A_1)
 end)
 
